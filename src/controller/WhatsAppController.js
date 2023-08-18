496892a5b887cd7672d2fb5fc6276cc044747155
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-18 09:14:06 -0300

    adiciona as configurações de inicio do menu de anexo, atraves do btnAttach

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -46,7 +46,7 @@ class WhatsAppController{
 
             Element.prototype.css = function(styles){
                 for (let name in styles){
-                    thiis.style[name] = styles[name]
+                    this.style[name] = styles[name]
                     return this;
 
                 }
@@ -74,6 +74,22 @@ class WhatsAppController{
         Element.prototype.hasClass = function (name){
           return this.classList.contains(name);
     }
+
+        HTMLFormElement.prototype.getForm = function(){
+
+            return new FormData(this);
+        }
+
+
+        HTMLFormElement.prototype.toJSON = function(){
+
+            let json ={};
+            this.getForm().forEach((value,key)=>{
+                json[key=value];
+            });
+            return json;
+        }
+
 }
 
     initEvents(){
@@ -89,17 +105,77 @@ class WhatsAppController{
 
         this.el.btnNewContact.on('click', e=>{
             this.closeAllLeftPanel();
-            this.el.panelAddContact.hide();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
             this.el.panelAddContact.addClass('open');
+        },300);
+            
         });
 
         this.el.btnClosePanelEditProfile.on('click', e=>{
             this.el.panelEditProfile.removeClass('open');
-        });
+        }); 
 
         this.el.btnClosePanelAddContact.on('click', e=>{
             this.el.panelAddContact.removeClass('open');
         });
+        this.el.photoContainerEditProfile.on('click', e=>{
+            this.el.inputProfilePhoto.click();
+        });
+        this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
+            if (e.key === 'Enter'){
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        })
+
+        this.el.btnSavePanelEditProfile.on('click', e=>{
+            console.log(this.el.inputNamePanelEditProfile);
+        });
+
+        this.el.formPanelAddContact.on('subimit',e=>{
+
+            e.preventDefault();
+            let formData = new FormData(this.el.formPanelAddContact);
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=>{
+
+            item.on('click', e=>{
+            
+            this.el.home.hide();
+            this.el.main.css({
+                display:'flex'
+            });
+        });
+    });
+
+    this.el.btnAttach.on('click', event => {
+
+        event.stopPropagation();
+
+        this.el.menuAttach.addClass('open');
+
+        document.addEventListener('click', this.closeMenuAttach.bind(this));
+    });
+
+    this.el.btnAttachPhoto.on('click', event => {
+
+    });
+
+    this.el.btnAttachCamera.on('click', event => {
+        
+    });
+
+    this.el.btnAttachDocument.on('click', event => {
+        
+    });
+    this.el.btnAttachContact.on('click', event => {
+        
+    });
+
+
 
     }
 
@@ -107,6 +183,13 @@ class WhatsAppController{
         this.el.panelAddContact.hide();
         this.el.panelEditProfile.hide();
         
+
+    }
+
+    closeMenuAttach(e) {
+
+        this.el.menuAttach.removeClass('open');
+        document.removeEventListener('click', this.closeMenuAttach);
 
     }
 
