004d9eb73e428192cce1bc1e2e8614e988e9a12e
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-21 11:32:23 -0300

    Adiciona DocumentPreviewController e getPreviewData para exibir pre vizualização do arquivo

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,6 +1,7 @@
 
 import {Format} from '../util/Format';
 import {CameraController} from './CameraController';
+import {DocumentPreviewController} from './DocumentPreviewController';
 
 export class WhatsAppController{
 
@@ -237,13 +238,44 @@ export class WhatsAppController{
         if(this.el.inputDocument.files.length){
 
             let file = this.el.inputDocument.files[0];
-            
-            this.panelDocumentPreviewController.getPriviewData().then(data=>{
 
-                console.log('ok',data);
+            this._documentPreviewController = new DocumentPreviewController(file)
+            
+            this._documentPreviewController.getPreviewData().then(result=>{
+
+            this.el.imgPanelDocumentPreview.src = result.src;
+            this.el.infoPanelDocumentPreview.innerHTML = result.info    
+            this.el.imagePanelDocumentPreview.show(); 
+            this.el.filePanelDocumentPreview.hide(); 
+
+
             }).catch(err=>{
                 console.log('err',err);
             })
+
+            switch (file.type) {
+                case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                case 'application/msword':
+                    this.el.iconPanelDocumentPreview.classList.value = 'jcxhw icon-doc-doc';
+                    break;
+
+                case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                case 'application/vnd.ms-excel':
+                    this.el.iconPanelDocumentPreview.classList.value = 'jcxhw icon-doc-xls';
+                    break;
+
+                case 'application/vnd.ms-powerpoint':
+                case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+                    this.el.iconPanelDocumentPreview.classList.value = 'jcxhw icon-doc-ppt';
+                    break;
+
+                default:
+                    this.el.iconPanelDocumentPreview.classList.value = 'jcxhw icon-doc-generic';
+            }
+
+            this.el.filePanelDocumentPreview.show();
+            this.el.imagePanelDocumentPreview.hide();
+           this.el.filenamePanelDocumentPreview.innerHTML = file.name;
 
         }
     })
