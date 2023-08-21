3fe206f85b99631d17e4b85b870bc6c840525d6b
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-21 12:27:13 -0300

    adiciona pre vizualização da 1º pagina do pdf

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -1,3 +1,8 @@
+const pdfjsLib = require('pdfjs-dist');
+const path = require ('path');
+
+pdfjsLib.GlobalWorkerOptions.workerSrc =  path.resolve(__dirname,'../../dist/pdf.worker.bundle.js');
+
 export class DocumentPreviewController{
     constructor(file){
         this._file = file;
@@ -7,6 +12,7 @@ export class DocumentPreviewController{
     getPreviewData(){
 
         return new Promise ((s,f)=>{
+            let reader = new FileReader();
 
             switch(this._file.type){
 
@@ -14,7 +20,7 @@ export class DocumentPreviewController{
                 case 'image/jpeg':
                 case 'image/jpg':
                 case 'image/gif':
-                let reader = new FileReader();
+
                 reader.onload = e =>{
 
                     s({
@@ -29,9 +35,51 @@ export class DocumentPreviewController{
                 }
                 reader.readAsDataURL(this._file);
 
-                    break;
+                break;
                 case 'application/pdf':
-                    break;
+                
+                reader.onload = e =>{
+
+                    pdfjsLib.getDocument(new Uint8Array(reader.result)).then( pdf =>{
+
+                        pdf.getPage(1).then(page=>{
+
+                            let viewport = page.getViewport(1);
+                            let canvas = document.createElement('canvas');
+                            let canvasContext = canvas.getContext('2d');
+
+                            canvas.width = viewport.width;
+                            canvas.height = viewport.height; 
+
+                            page.render({
+                                canvasContext,
+                                viewport
+
+                            }).then(()=>{
+
+                                let _s = (pdf.numPages >1) ? 's' : '';
+
+                                s({
+                                    src: canvas.toDataURL('image/png'),
+                                    info: `${pdf.numPages} pagina${_s}`
+                                })
+
+                            }).catch(err=>{
+                                f(err);
+                            });
+
+                        })
+
+                    }).catch(err=>{
+                        f(rer);
+                    })
+
+                }
+
+                reader.readAsArrayBuffer(this._file);
+
+                
+                break;
                 default:
                     f();
             }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -237,6 +237,10 @@ export class WhatsAppController{
 
         if(this.el.inputDocument.files.length){
 
+            this.el.panelDocumentPreview.css({
+                'height': '1%'
+            });
+
             let file = this.el.inputDocument.files[0];
 
             this._documentPreviewController = new DocumentPreviewController(file)
@@ -248,9 +252,15 @@ export class WhatsAppController{
             this.el.imagePanelDocumentPreview.show(); 
             this.el.filePanelDocumentPreview.hide(); 
 
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)'
+            });
 
             }).catch(err=>{
-                console.log('err',err);
+                
+                this.el.panelDocumentPreview.css({
+                    'height': 'calc(100% - 120px)'
+                });
             })
 
             switch (file.type) {
