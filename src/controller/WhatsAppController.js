331f93aca19fa03c6f00c64250bd32278e412834
giovanne <giovanne.aquino.clear@gmail.com>
2023-08-24 12:09:03 -0300

    Altera setActiveChat

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -155,18 +155,25 @@ export class WhatsAppController{
                 display:'flex'
             });
 
+                
+            this.el.panelMessagesContainer.innerHTML = '';
+
             Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
             .onSnapshot(docs =>{
              
-                this.el.panelMessagesContainer.innerHTML = '';
-                
+ 
+         
+                let scrollTop = this.el.panelMessagesContainer.scrollTop;
+                let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+                let autoScroll = (scrollTop >= scrollTopMax);
+
                 docs.forEach(doc => {
 
                     let data = doc.data();
                     data.id = doc.id;
 
                     if(!this.el.panelMessagesContainer.querySelector('#_' + data.id))
-                    {
+                    {   
 
                     let message = new Message();
 
@@ -179,7 +186,17 @@ export class WhatsAppController{
                     this.el.panelMessagesContainer.appendChild(view);
                 }
 
-                });
+             });
+
+                if(autoScroll){
+                    this.el.panelMessagesContainer.scrollTop = 
+                    (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight)
+                }else{
+                
+                    this.el.panel.panelMessagesContainer.scrollTop = scrollTop;
+
+                }
+
             });
 
         }
